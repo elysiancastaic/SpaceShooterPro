1eb79d3d1a3284672aca14d0e775180efa474398
elysiancastaic <tlhutchings88@gmail.com>
2022-10-19 09:57:35 -0700

    Player Movement Script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //public or private reference
-    //data type (int, float, bool, string)
-    //every variable has a name
-    //optional value assigned
     [SerializeField]
+
     private float _speed = 3.5f;
 
     // Start is called before the first frame update
@@ -16,11 +13,24 @@ public class Player : MonoBehaviour
     {
         // take current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        //new Vector3(1, 0, 0) * 3.5f * reltime
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+        
     }
 }
