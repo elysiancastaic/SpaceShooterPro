4d4c8f5da3b389df6e091154ee7d6241eef3ba8a
elysiancastaic <tlhutchings88@gmail.com>
2022-10-21 17:39:33 -0700

    Player Bounds and Movement Finished

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,9 @@ public class Player : MonoBehaviour
 
     private float _speed = 3.5f;
 
-    // Start is called before the first frame update
+   // Start is called before the first frame update
     void Start()
     {
-        // take current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
 
     }
@@ -19,18 +18,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CalculateMovement(); 
+    }
+
+    void CalculateMovement()
+    {
+
         float horizontalInput = Input.GetAxis("Horizontal");
 
         float verticalInput = Input.GetAxis("Vertical");
-
-        //new Vector3(1, 0, 0) * 3.5f * reltime
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
         Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
-        
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+
+        }
+
     }
 }
